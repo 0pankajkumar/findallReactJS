29bd1383d8281ba4657522473a9c8a6937795557
Pankaj Kumar <805.bluebell@gmail.com>
2019-02-12 20:49:31 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+  
+  render() {
+    return (
+    <>
+      <form onSubmit={this.handleSubmit}>
+          <label>
+          Enter Paragraph:
+          <textarea name="paragraph" rows="10" cols="30" value={this.state.value} onChange={this.handleChange}> 
+          </textarea>
+          </label>
+      </form>
+    </>
+    );
+  }
+}
+
+
+
+class InputWord extends React.Component {
+  constructor(props) {
+
+  }
+
+  render() {
+    return (
+    <>
+      <form onSubmit={this.handleSubmit}>
+          
+          <label>
+          Enter Paragraph:
+          <textarea name="paragraph" rows="10" cols="30" value={this.state.value} onChange={this.handleChange}> 
+          </textarea>
+          </label>
+
+          <label>
+          Enter Word/s to be searched:
+          <input type="text" name="word" value={this.state.value} onChange={this.handleChange} /> 
+          </label>
+          
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+    );
+  }
+}
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="area">
+        <div className="input-para">
+          <InputPara />
+        </div>
+        <div className="input-word">
+          <InputWord />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
